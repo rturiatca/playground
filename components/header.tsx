0f2f2a79623b49774e4b41f3758add23e7557148
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2024-12-27 13:30:18 +0200

    prepare for deployment

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,14 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import {  usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import parse from 'html-react-parser';
 // import Tooltip from './tool-tip';
 import { onEntryChange } from '../contentstack-sdk';
 import { getAllEntries, getHeaderRes } from '../helper';
 import Skeleton from 'react-loading-skeleton';
-import { HeaderProps, Entry, NavLinks } from "../typescript/layout";
+import { HeaderProps, Entry, NavLinks } from '../typescript/layout';
 
 export default function Header() {
   const [header, setHeaderProp] = useState<HeaderProps | undefined>(undefined);
@@ -21,36 +21,36 @@ export default function Header() {
     const entriesRes = await getAllEntries();
     setHeaderProp(headerRes);
     setEntries(entriesRes);
-  }
+  };
 
   function buildNavigation(ent: Entry, hd: HeaderProps) {
-    let newHeader={...hd};
-    if (ent.length!== newHeader.navigation_menu.length) {
-          ent.forEach((entry) => {
-            const hFound = newHeader?.navigation_menu.find(
-              (navLink: NavLinks) => navLink.label === entry.title
-            );
-            if (!hFound) {
-              newHeader.navigation_menu?.push({
-                label: entry.title,
-                page_reference: [
-                  { title: entry.title, url: entry.url, $: entry.$ },
-                ],
-                $:{}
-              });
-            }
+    let newHeader = { ...hd };
+    if (ent.length !== newHeader.navigation_menu.length) {
+      ent.forEach((entry) => {
+        const hFound = newHeader?.navigation_menu.find(
+          (navLink: NavLinks) => navLink.label === entry.title
+        );
+        if (!hFound) {
+          newHeader.navigation_menu?.push({
+            label: entry.title,
+            page_reference: [
+              { title: entry.title, url: entry.url, $: entry.$ },
+            ],
+            $: {},
           });
+        }
+      });
     }
-    return newHeader
+    return newHeader;
   }
 
   async function fetchData() {
     try {
       if (header && entries) {
-      const headerRes = await getHeaderRes();
-      const newHeader = buildNavigation(entries,headerRes)
-      setHeader(newHeader);
-    }
+        const headerRes = await getHeaderRes();
+        const newHeader = buildNavigation(entries, headerRes);
+        setHeader(newHeader);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -66,13 +66,13 @@ export default function Header() {
     }
   }, [header]);
   const headerData = getHeader ? getHeader : undefined;
-  
+
   return (
-    <header className='header'>
-      <div className='note-div'>
+    <header className="header">
+      <div className="note-div">
         {headerData?.notification_bar.show_announcement ? (
           typeof headerData.notification_bar.announcement_text === 'string' && (
-            <div {...headerData.notification_bar.$?.announcement_text as {}}>
+            <div {...(headerData.notification_bar.$?.announcement_text as {})}>
               {parse(headerData.notification_bar.announcement_text)}
             </div>
           )
@@ -80,17 +80,17 @@ export default function Header() {
           <Skeleton />
         )}
       </div>
-      <div className='max-width header-div'>
-        <div className='wrapper-logo'>
+      <div className="max-width header-div">
+        <div className="wrapper-logo">
           {headerData ? (
-            <Link legacyBehavior href='/'>
-              <a className='logo-tag' title='Contentstack'>
+            <Link legacyBehavior href="/">
+              <a className="logo-tag" title="Contentstack">
                 <img
-                  className='logo'
+                  className="logo"
                   src={headerData.logo.url}
                   alt={headerData.title}
                   title={headerData.title}
-                  {...headerData.logo.$?.url as {}}
+                  {...(headerData.logo.$?.url as {})}
                 />
               </a>
             </Link>
@@ -98,12 +98,12 @@ export default function Header() {
             <Skeleton width={150} />
           )}
         </div>
-        <input className='menu-btn' type='checkbox' id='menu-btn' />
-        <label className='menu-icon' htmlFor='menu-btn'>
-          <span className='navicon' />
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn">
+          <span className="navicon" />
         </label>
-        <nav className='menu'>
-          <ul className='nav-ul header-ul'>
+        <nav className="menu">
+          <ul className="nav-ul header-ul">
             {headerData ? (
               headerData.navigation_menu.map((list) => {
                 const className =
@@ -111,8 +111,8 @@ export default function Header() {
                 return (
                   <li
                     key={list.label}
-                    className='nav-li'
-                    {...list.page_reference[0].$?.url as {}}
+                    className="nav-li"
+                    {...(list.page_reference[0].$?.url as {})}
                   >
                     <Link legacyBehavior href={list.page_reference[0].url}>
                       <a className={className}>{list.label}</a>
--- components/hero-banner.tsx
+++ components/hero-banner.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import { Image, Action } from "../typescript/action";
+import { Image, Action } from '../typescript/action';
 
 type AdditionalParam = {
   banner_title: string;
   banner_description: string;
-}
+};
 
 type Banner = {
   bg_color: string;
@@ -15,41 +15,50 @@ type Banner = {
   call_to_action: Action;
   banner_image: Image;
   $: AdditionalParam;
-}
+};
 
 type BannerProps = {
   banner: Banner;
-}
+};
 
 export default function HeroBanner(props: BannerProps) {
-
   const banner = props.banner;
 
   return (
     <div
-      className='hero-banner'
+      className="hero-banner"
       style={{
         background: banner?.bg_color ? banner.bg_color : '',
       }}
     >
+      {banner.banner_image ? (
+        <img
+          className="hero-image"
+          alt={banner.banner_image.filename}
+          src={banner.banner_image.url}
+          {...(banner.banner_image.$?.url as {})}
+        />
+      ) : (
+        ''
+      )}
       <div
-        className='home-content'
+        className="home-content"
         style={{
           color: banner?.text_color ? banner.text_color : '#000',
         }}
       >
         {banner.banner_title && (
-          <h1 className='hero-title' {...banner.$?.banner_title as {}}>
+          <h1 className="hero-title" {...(banner.$?.banner_title as {})}>
             {banner.banner_title}
           </h1>
         )}
         {banner.banner_description ? (
           <p
-            className='hero-description '
+            className="hero-description "
             style={{
               color: banner?.text_color ? banner.text_color : '#222',
             }}
-            {...banner.$?.banner_description as {}}
+            {...(banner.$?.banner_description as {})}
           >
             {banner?.banner_description}
           </p>
@@ -58,7 +67,7 @@ export default function HeroBanner(props: BannerProps) {
         )}
         {banner.call_to_action.title && banner.call_to_action.href ? (
           <Link legacyBehavior href={banner?.call_to_action.href}>
-            <a className='btn tertiary-btn' {...banner.call_to_action.$?.title}>
+            <a className="btn tertiary-btn" {...banner.call_to_action.$?.title}>
               {banner?.call_to_action.title}
             </a>
           </Link>
@@ -66,15 +75,6 @@ export default function HeroBanner(props: BannerProps) {
           ''
         )}
       </div>
-      {banner.banner_image ? (
-        <img
-          alt={banner.banner_image.filename}
-          src={banner.banner_image.url}
-          {...banner.banner_image.$?.url as {}}
-        />
-      ) : (
-        ''
-      )}
     </div>
   );
 }
